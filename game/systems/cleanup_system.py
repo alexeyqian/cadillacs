3b1b4fbdec22c5adea762d182fe552048dc7fd61
Alexey <alexey@example.com>
2026-06-05 12:53:26 +0900

    refactory

---
+++ game/systems/cleanup_system.py
@@ -0,0 +1,19 @@
+def cleanup_game_state(game_state):
+    # remove dead enemies
+    game_state.enemies[:] = [
+        enemy for enemy in game_state.enemies
+        if not enemy.is_ready_to_remove()
+    ]
+    # clean up player projectiles
+    game_state.projectiles[:] = [p for p in game_state.projectiles if p.active]
+    # clean up enemy projectiles
+    game_state.enemy_projectiles[:] = [
+        p for p in game_state.enemy_projectiles
+        if p.active
+    ]
+    # clean up breakables
+    game_state.objects[:] = [obj for obj in game_state.objects if obj.hp > 0]
+    # clean up loots
+    game_state.loot_items[:] = [l for l in game_state.loot_items if l.active]
+    # clean up hit sparks
+    game_state.hit_sparks[:] = [s for s in game_state.hit_sparks if s.active]
--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from game.entities.enemy import Enemy
 from game.entities.weapon import Weapon
 from game.entities.loot import Loot
 from game.entities.breakable_object import BreakableObject
-from main_update import *
-from main_draw import *
 from game.effects.hit_spark import HitSpark
 from game.game_state import GameState
 from game.systems.inventory_system import *
@@ -17,6 +15,8 @@ from game.systems.wave_system import *
 from game.systems.loot_system import *
 from game.systems.projectile_system import *
 from game.systems.combat_system import *
+from game.systems.cleanup_system import *
+from main_draw import *
 
 def create_enemy_rect(enemy):
     return pygame.Rect(enemy.x, enemy.y,
@@ -80,7 +80,36 @@ def main():
         update_player_weapon_interaction(player, weapons, keys)
 
         ############# update #############
-        main_update(game_state)
+        # update player
+        player.update()
+        collect_player_projectiles(game_state)
+
+        # should move to player's own update() function
+        # prevent escaping arena
+        if level.camera_locked:
+            left_wall = level.lock_x
+            right_wall = level.lock_x + SCREEN_WIDTH - player.width
+            if player.x < left_wall:
+                player.x = left_wall
+            if player.x > right_wall:
+                player.x = right_wall
+
+        # update enemies
+        for enemy in enemies:
+            enemy.update(player, enemies)
+            collect_enemy_projectile(game_state, enemy)
+
+        update_projectiles(game_state)
+            
+        # update hit sparks
+        for spark in hit_sparks:
+            spark.update()
+
+        # update camera
+        if level.camera_locked:
+            camera.update(player, level.lock_x)
+        else:
+            camera.update(player)
 
         handle_player_attack_collision(game_state)
         handle_player_projectile_collision(game_state)
@@ -91,10 +120,9 @@ def main():
         # and before dead enemy and broken object removed
         create_enemy_loot(game_state)
         create_object_loot(game_state)
-
         update_loot_pickup(game_state)
 
-        main_cleanup(game_state)
+        cleanup_game_state(game_state)
         update_wave_completion(game_state)
         main_draw(game_state)
 
@@ -102,26 +130,6 @@ def main():
         clock.tick(FPS)
 
     pygame.quit()
-
-def main_cleanup(game_state):
-        # remove dead enemies
-        game_state.enemies[:] = [
-            enemy for enemy in game_state.enemies
-            if not enemy.is_ready_to_remove()
-        ]
-        # clean up player projectiles
-        game_state.projectiles[:] = [p for p in game_state.projectiles if p.active]
-        # clean up enemy projectiles
-        game_state.enemy_projectiles[:] = [
-            p for p in game_state.enemy_projectiles
-            if p.active
-        ]
-        # clean up breakables
-        game_state.objects[:] = [obj for obj in game_state.objects if obj.hp > 0]
-        # clean up loots
-        game_state.loot_items[:] = [l for l in game_state.loot_items if l.active]
-        # clean up hit sparks
-        game_state.hit_sparks[:] = [s for s in game_state.hit_sparks if s.active]
 
 if __name__ == "__main__":
     main()
