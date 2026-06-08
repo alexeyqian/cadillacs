b0879581aab0ba535db0849f01109edeff0962e7
Alexey <alexey@example.com>
2026-06-09 00:44:14 +0900

    add offset

--- game/level/background.py
+++ game/level/background.py
@@ -2,25 +2,13 @@ import pygame
 from game.assets.asset_manager import AssetManager
 
 class Background:
-    def __init__(
-        self,
-        far_file,
-        mid_file,
-        front_file
-    ):
+    def __init__(self,far_file,mid_file,front_file):
         self.far = AssetManager.load_image(far_file, alpha=False)
         self.mid = AssetManager.load_image(mid_file, alpha=True)
         self.front = AssetManager.load_image(front_file, alpha=True)
 
-    def draw_layer(
-        self,
-        screen,
-        image,
-        camera_x,
-        scroll_speed
-    ):
+    def draw_layer(self,screen,image,camera_x,scroll_speed, offset_y=0):
         image_width = image.get_width()
-
         x = -camera_x * scroll_speed
 
         # repeat image horizontally
@@ -28,36 +16,13 @@ class Background:
             x -= image_width
 
         while x < screen.get_width():
-            screen.blit(image, (x, 0))
+            # todo: remove the offset
+            screen.blit(image, (x, 0+offset_y))
             x += image_width
 
-    def draw_back(
-        self,
-        screen,
-        camera_x
-    ):
-        self.draw_layer(
-            screen,
-            self.far,
-            camera_x,
-            0.25
-        )
+    def draw_back(self,screen,camera_x):
+        self.draw_layer(screen,self.far,camera_x,0.25)
+        self.draw_layer(screen,self.mid,camera_x,0.55, 120)
 
-        self.draw_layer(
-            screen,
-            self.mid,
-            camera_x,
-            0.55
-        )
-
-    def draw_front(
-        self,
-        screen,
-        camera_x
-    ):
-        self.draw_layer(
-            screen,
-            self.front,
-            camera_x,
-            0.85
-        )
+    def draw_front(self,screen,camera_x):
+        self.draw_layer(screen,self.front,camera_x,0.85)
