02085a9ef8a5b82feddf8e5f6356b7dbe63f0a5b
Alexey <alexey@example.com>
2026-06-14 11:27:58 +0900

    refactory

--- game/entities/enemy.py
+++ game/entities/enemy.py
@@ -1,13 +1,9 @@
 import random
-import pygame
 
 from game.settings import *
-from game.colors import *
 from game.entities.loot import Loot
 
-from game.assets.placeholder.enemy_frames import *
 from game.animation.animation_manager import AnimationManager
-from game.animation.animation_config import *
 from game.tuning import scale_frames, scale_timing
 from game.entities.enemy_state import EnemyState
 from game.entities.enemy_boxes import EnemyBoxMixin
@@ -15,8 +11,11 @@ from game.entities.enemy_ai import EnemyAIMixin
 from game.entities.enemy_combat import EnemyCombatMixin
 from game.entities.enemy_animation import EnemyAnimationMixin
 from game.entities.enemy_render import EnemyRendererMixin
+from game.entities.enemy_lifecycle import EnemyLifecycleMixin
+from game.entities.enemy_reactions import EnemyReactionMixin
 
 class Enemy(EnemyBoxMixin, EnemyAIMixin, EnemyCombatMixin,
+            EnemyReactionMixin, EnemyLifecycleMixin,
             EnemyAnimationMixin, EnemyRendererMixin):
     IDLE = EnemyState.IDLE
     WALK = EnemyState.WALK
@@ -156,82 +155,6 @@ class Enemy(EnemyBoxMixin, EnemyAIMixin, EnemyCombatMixin,
         self.attack_hitbox_h = int(self.height * 0.5)
         self.attack_hitbox_offset_y = int(self.height * 0.2)
 
-    def update_special_states(self):
-        if self.state == self.GRABBED:
-            self.update_animation()
-            return True
-        if self.state == self.THROWN:
-            self.update_thrown_state()
-            return True
-        if self.state == self.KNOCKDOWN:
-            self.update_knockdown_state()
-            return True
-        if self.state == self.GETUP:
-            self.update_getup_state()
-            return True
-        if self.state == self.DEAD:
-            self.update_dead_state()
-            return True
-
-        return False
-
-    def update_thrown_state(self):
-        if self.thrown_velocity_x > 0:
-                self.facing_right = True
-        elif self.thrown_velocity_x < 0:
-            self.facing_right = False
-
-        self.x += self.thrown_velocity_x
-        self.thrown_velocity_x *= 0.9
-        self.thrown_timer -= 1
-        
-        if self.thrown_timer <= 0 or abs(self.thrown_velocity_x) < 1:
-            self.state = self.KNOCKDOWN
-            self.knockdown_timer = 60
-            self.thrown_velocity_x = 0
-
-        self.update_animation()
-
-    def update_knockdown_state(self):
-        self.knockdown_timer -= 1
-        if self.knockdown_timer <= 0:
-            self.state = self.GETUP
-            self.getup_timer = 20
-        self.update_animation()
-
-    def update_getup_state(self):
-        self.getup_timer -= 1
-        if self.getup_timer <= 0:
-            self.state = self.IDLE
-        self.update_animation()
-
-    def update_hit_state(self):
-        if self.hit_timer <= 0:
-            return False
-
-        # enemy pauses briefly when hit by player, but can still be knocked back
-        self.hit_timer -= 1
-        self.apply_knockback()
-        if self.hit_timer <= 0:
-            self.state = self.IDLE
-        else:
-            self.state = self.HIT
-        self.update_animation()
-
-        return True
-
-    def update_dead_state(self):
-        if not self.death_timer_started:
-            self.death_timer_started = True
-        if self.death_timer > 0:
-            self.death_timer -= 1
-        self.update_animation()
-
-    def update_timers(self):
-        # attack cooldown
-        if self.attack_cooldown > 0:
-            self.attack_cooldown -= 1
-
     def update(self, player, enemies):
         if self.update_special_states():
             return
@@ -246,10 +169,6 @@ class Enemy(EnemyBoxMixin, EnemyAIMixin, EnemyCombatMixin,
         self.execute_state(player, enemies, dx, dy)
         #self.apply_world_bounds()
         self.update_animation()
-
-
-    def is_ready_to_remove(self):
-        return self.state == self.DEAD and self.death_timer <= 0
 
     def create_loot(self):
         roll = random.randint(1, 100)
