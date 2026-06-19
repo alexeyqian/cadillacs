ce76d00cd9f431d1e134e35fc19416351c045119
Alexey <alexey@example.com>
2026-06-19 10:36:20 +0900

    refac

--- game/entities/enemy_factory.py
+++ game/entities/enemy_factory.py
@@ -2,17 +2,34 @@ from game.entities.ferris_enemy import FerrisEnemy
 from game.entities.gneiss_enemy import GneissEnemy
 from game.entities.black_elmer_enemy import BlackElmerEnemy
 from game.entities.walther_enemy import WaltherEnemy
+from game.entities.raptor_enemy import RaptorEnemy
+from game.entities.enemy_config import DEFAULT_ENEMY_TYPE
 
 class EnemyFactory:
+    enemy_classes = {
+        "ferris": FerrisEnemy,
+        "gneiss": GneissEnemy,
+        "black_elmer": BlackElmerEnemy,
+        "walther": WaltherEnemy,
+        "raptor": RaptorEnemy,
+    }
+
     @staticmethod
     def create_enemy(enemy_type, x, y):
-        if enemy_type == "ferris":
-            return FerrisEnemy(x, y)
-        if enemy_type == "gneiss":
-            return GneissEnemy(x, y)
-        if enemy_type == "black_elmer":
-            return BlackElmerEnemy(x, y)
-        if enemy_type == "walther":
-            return WaltherEnemy(x, y)
+        enemy_class = EnemyFactory.get_enemy_class(enemy_type)
+        return enemy_class(x, y)
 
-        return FerrisEnemy(x, y)
+    @staticmethod
+    def get_enemy_class(enemy_type):
+        return EnemyFactory.enemy_classes.get(
+            enemy_type,
+            EnemyFactory.enemy_classes[DEFAULT_ENEMY_TYPE],
+        )
+
+    @staticmethod
+    def register_enemy_type(enemy_type, enemy_class):
+        EnemyFactory.enemy_classes[enemy_type] = enemy_class
+
+    @staticmethod
+    def registered_enemy_types():
+        return tuple(EnemyFactory.enemy_classes.keys())
---
+++ tests/test_enemy_factory.py
@@ -0,0 +1,46 @@
+from game.entities.enemy_config import DEFAULT_ENEMY_TYPE, get_enemy_config
+from game.entities.enemy_factory import EnemyFactory
+from game.entities.ferris_enemy import FerrisEnemy
+from game.entities.raptor_enemy import RaptorEnemy
+
+
+class FakeEnemy:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def test_enemy_factory_uses_registered_enemy_classes():
+    original_classes = EnemyFactory.enemy_classes.copy()
+    try:
+        EnemyFactory.register_enemy_type("test_enemy", FakeEnemy)
+
+        enemy = EnemyFactory.create_enemy("test_enemy", 10, 20)
+
+        assert isinstance(enemy, FakeEnemy)
+        assert enemy.x == 10
+        assert enemy.y == 20
+    finally:
+        EnemyFactory.enemy_classes = original_classes
+
+
+def test_enemy_factory_falls_back_to_default_enemy_class():
+    assert EnemyFactory.get_enemy_class("unknown") is FerrisEnemy
+    assert EnemyFactory.get_enemy_class("unknown") is EnemyFactory.get_enemy_class(
+        DEFAULT_ENEMY_TYPE
+    )
+
+
+def test_enemy_factory_registers_raptor_enemy_type():
+    assert EnemyFactory.get_enemy_class("raptor") is RaptorEnemy
+    assert "raptor" in EnemyFactory.registered_enemy_types()
+
+
+def test_enemy_config_has_raptor_data_and_default_fallback():
+    raptor_config = get_enemy_config("raptor")
+    fallback_config = get_enemy_config("unknown")
+
+    assert raptor_config.enemy_id == "raptor"
+    assert raptor_config.display_name == "Raptor"
+    assert raptor_config.archetype == "leaper"
+    assert fallback_config.enemy_id == DEFAULT_ENEMY_TYPE
